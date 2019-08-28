a38a8f18f0008f7f3886e77bff7654106571102b
Huang, Wenfeng <Wenfeng.Huang@desay-svautomotive.com>
2019-08-28 18:23:22 +0800

    后台模板：DesignBootstrap

---
+++ 前后台模板整理合集/backend/DesignBootstrap/assets/js/custom.js
@@ -0,0 +1,42 @@
+﻿/*=============================================================
+    Authour URL: www.designbootstrap.com
+    
+    http://www.designbootstrap.com/
+
+    License: MIT
+
+    http://opensource.org/licenses/MIT
+
+    100% Free To use For Personal And Commercial Use.
+
+    IN EXCHANGE JUST TELL PEOPLE ABOUT THIS WEBSITE
+   
+========================================================  */           
+
+$(document).ready(function () {
+
+    /*====================================
+           METIS MENU 
+     ======================================*/
+
+    $('#main-menu').metisMenu();
+
+    /*======================================
+    LOAD APPROPRIATE MENU BAR ON SIZE SCREEN
+    ========================================*/
+
+    $(window).bind("load resize", function () {
+        if ($(this).width() < 768) {
+            $('div.sidebar-collapse').addClass('collapse')
+        } else {
+            $('div.sidebar-collapse').removeClass('collapse')
+        }
+    });
+    /*======================================
+   WRITE YOUR SCRIPTS BELOW
+   ========================================*/
+   
+   
+
+});
+      
